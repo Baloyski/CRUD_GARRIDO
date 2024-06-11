815422f7bfb49bab035b3c4db211175e40e3f0df
Baloyski <144868160+Baloyski@users.noreply.github.com>
2024-06-12 02:59:09 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDO4ZLWqBeODMW5wOrjw2VnceQLPqmEaEg",
+  authDomain: "crud-1d637.firebaseapp.com",
+  databaseURL: "https://crud-1d637-default-rtdb.firebaseio.com",
+  projectId: "crud-1d637",
+  storageBucket: "crud-1d637.appspot.com",
+  messagingSenderId: "52382106166",
+  appId: "1:52382106166:web:280e96a7ccafde50d74e97"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
